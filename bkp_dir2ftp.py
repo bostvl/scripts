2783a49c9fd9d65109266501877b9be4b73c986e
Charlie Root <root@e-learning.lnu.edu.ua>
2016-06-06 11:07:02 +0300

    worked

--- bkp_dir2ftp.py
+++ bkp_dir2ftp.py
@@ -6,8 +6,9 @@ from ftplib import FTP
 config = ConfigParser.ConfigParser()
 config.read("/root/scripts/bkp_dir.conf")
 
-LocalDIR=config.get('conf','LocalDIR')
-RemoteDIR=config.get('conf','RemoteDIR')
+LocalDIR=os.path.abspath(config.get('conf','LocalDIR'))
+RemoteDIR=os.path.abspath(config.get('conf','RemoteDIR'))
+
 if len(sys.argv)==1:
 	BackupDIR=os.path.abspath(config.get('conf','BackupDIR'))
 else:
@@ -16,15 +17,9 @@ else:
 if not os.path.exists(BackupDIR):
 	print "ERROR!!! Directory not exixts:"+BackupDIR
 	sys.exit(1)
-if not os.path.isdir(BackupDIR)
+if not os.path.isdir(BackupDIR):
 	print "ERROR!!! It is not a directory: "+BackupDIR
 	sys.exit(1)
-
-
-
-
-
-
 
 FTPHost=config.get('conf','FTPHost')
 FTPUser=config.get('conf','FTPUser')
@@ -42,45 +37,48 @@ Y=str(now.year)
 M=str(now.month)
 D=str(now.day)
 DIR=os.path.join(LocalDIR,Y,M,D)
-FILE=os.path.basename(BackupDIR)+"_"+now.strftime("%Y%m%d_%H%M")
+FILE=os.path.basename(BackupDIR)+"_"+now.strftime("%Y%m%d_%H%M")+".tgz"
+TARFILE=os.path.join(DIR,FILE)
 
-print DIR
-print FILE
 
-sys.exit()
+#*********    Creating local copy *************************************************
+
+if not os.path.exists(DIR):
+        os.makedirs(DIR)
+try:
+	tarGZ = tarfile.open(TARFILE,'w:gz')
+	tarGZ.add(BackupDIR)
+	tarGZ.close()
+	#f.write('tar end\n')
+	#f.close()
+	print "OK"	
+except tarfile.TarError, tarexc:
+	#f.write('exception error')
+	#f.close()
+	print tarexc
+
+
 #Creating RemoteDIR structure ******************************************************
 ftp=FTP(FTPHost,FTPUser,FTPPassword)
 ftp.cwd(RemoteDIR)
 try:
-	ftp.mkd(RemoteDIR+Y)
+	ftp.mkd(os.path.join(RemoteDIR,Y))
 except Exception,e:
 	print e
 try:
-	ftp.mkd(RemoteDIR+Y+"/"+M)
+	ftp.mkd(os.path.join(RemoteDIR,Y,M))
 except Exception,e:
 	print e
 try:
-	ftp.mkd(RemoteDIR+Y+"/"+M+"/"+D)
+	ftp.mkd(os.path.join(RemoteDIR,Y,M,D))
 except Exception,e:
 	print e
 
-if not os.path.exists(DIR):
-	os.makedirs(DIR)
-#with open(DIR+FILE, 'rb') as f_in, gzip.open(DIR+FILE+".gz", 'wb') as f_out:  #gzip file
-#	shutil.copyfileobj(f_in, f_out)
-os.remove(DIR+FILE) #Delete old file
-FILE=FILE+".gz"
-
-
-rDIR=RemoteDIR+Y+"/"+M+"/"+D+"/"
+rDIR=os.path.join(RemoteDIR,Y,M,D);
 ftp.cwd(rDIR)
-ftp.storbinary('STOR '+FILE, open(DIR+FILE, 'rb'))
+ftp.storbinary('STOR '+os.path.basename(TARFILE), open(TARFILE, 'rb'))
 
 ftp.quit()
 
 print "OK"
 
-
-
-#/usr/bin/ssh univer@194.44.10.35 /home/univer/make_dir.sh
-#/usr/bin/scp $DIR/$FILE univer@194.44.10.35:/home/univer/bkp/sql/$Y/$M/$D
